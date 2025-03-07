e9fd1fe917078083d069e346cfad70b3e6c53c56
AgusMS7 <agustin.matias.sandoval@gmail.com>
2025-03-07 20:43:32 -0300

    PrimerCommit

---
+++ src/clases_propias/HerenciaGenericos.java
@@ -0,0 +1,24 @@
+package clases_propias;
+
+
+public class HerenciaGenericos {
+
+    public static void main(String[] args) {
+        
+//        Empleado Administrativa = new Empleado("Elena",45,1500);
+//        
+//        Jefe DirectoraComercial = new Jefe("Ana",27,3500);
+//        
+//        Empleado nuevoEmpleado = DirectoraComercial;
+    
+        Pareja<Empleado> Administrativa = new Pareja<Empleado>();
+        
+        Pareja<Jefe> DirectoraComercial = new Pareja<Jefe>();
+        
+//        Pareja<Empleado> nuevoEmpleado = DirectoraComercial;
+
+        Pareja.imprimirTrabajador(Administrativa);
+        
+        Pareja.imprimirTrabajador(DirectoraComercial);
+    }
+}
